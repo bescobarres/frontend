a8abab54135e6b78af02ce0e50800b40c3ad61a4
Bryan <bescobarres@gmail.com>
2018-08-09 01:33:23 -0500

    Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ListaVehiculosComponent } from './lista-vehiculos/lista-vehiculos.component';
 import { FacturaService } from './_service/factura/FacturaService';
 import { HttpClientModule } from '@angular/common/http';
 import { Config } from './Config';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaVehiculosComponent
+    FormsModule,
+    ListaVehiculosComponent,
+    HeaderComponent,
+    FooterComponent,
+    RouterModule
   ],
   imports: [
     BrowserModule,
